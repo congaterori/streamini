587f70f9a8193fb418ce4cddb6c0410619c2d449
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2024-10-05 18:56:44 -0700

    Update start.bat add :: for notes LOL :P
    Update start.bat add :: for notes LOL :P  make it faster 0.00000....1 second

--- start.bat
+++ start.bat
@@ -1,11 +1,11 @@
 @echo off
-REM Check if a port is provided as an argument
+:: Check if a port is provided as an argument
 if "%1"=="" (
   set port=8502  REM Default port
 ) else (
   set port=%1
 )
-REM Execute the streamlit command with the specified or default port
+:: Execute the streamlit command with the specified or default port
 echo Streamlit starting on port %port%...
 streamlit run --server.port %port% app.py
 pause
